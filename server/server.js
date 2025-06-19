d0eba9cf39630a6cf6f6c246ff256e7e8ae96ff4
HARI <ahsk321@gmail.com>
2025-06-19 23:59:47 +0530

    added vercel configuration

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 });
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+}));
 app.use(express.json());
 app.use('/api/auth',authRoutes);
 app.use('/api/faculty',facultyRoutes);
